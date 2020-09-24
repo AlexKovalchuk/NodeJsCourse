e8cc498aad631e3f67e7e8f68b87e7b0a4935d19
OleksandrKovalchuk <oleksandr.kovalchuk@cerence.com>
2020-09-24 18:05:28 +0300

    lesson 12

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    console.log(req.url)
+
+    res.write('<h1>Hello from NodeJS</h1>')
+    res.end(`
+        <div style="background: red; width:500px; height: 400px; text-align: center; font-size:30px;">The End</div>
+    `);
+})
+
+server.listen(3000, () => {
+    console.log('Server is running...')
+});
