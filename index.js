933aa5371f3560aa71f690850286c4c015742e92
OleksandrKovalchuk <oleksandr.kovalchuk@cerence.com>
2020-09-28 17:50:21 +0300

    nodemon

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http')
 const server = http.createServer((req, res) => {
     console.log(req.url)
 
-    res.write('<h1>Hello from NodeJS</h1>')
+    res.write('<h1>Aelx, Hello from NodeJS</h1>')
     res.end(`
         <div style="background: red; width:500px; height: 400px; text-align: center; font-size:30px;">The End</div>
     `);
