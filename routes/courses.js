96f0ca1fd3f3ccee3b005cee4efa8ec7b3b91769
OleksandrKovalchuk <oleksandr.kovalchuk@cerence.com>
2020-10-12 16:31:33 +0300

    28

--- routes/courses.js
+++ routes/courses.js
@@ -1,12 +1,17 @@
 const {Router} = require('express');
+const Course = require('../models/course')
 
 const router = Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const courses = await Course.getAll();
+    console.log('courses', courses)
     res.render('courses', {
         title: 'Courses',
-        isCourses: true
+        isCourses: true,
+        courses
     })
+   
 })
 
 module.exports = router;
