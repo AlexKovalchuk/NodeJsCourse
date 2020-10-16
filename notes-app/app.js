833983cb47535309c78cefeff9b55ea44a9006e7
OleksandrKovalchuk <oleksandr.kovalchuk@cerence.com>
2020-10-16 17:35:21 +0300

    init

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,3 @@
 const {getNotes} = require('./notes');
 
 console.log(getNotes());
-
-
-
-// const {name, add} = require('./utils')
-// console.log('Hello',name)
-// console.log('5 + 7 =', add(5,7))
