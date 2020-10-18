0ca3acc905825fec95194429084534d01064bf60
OleksandrKovalchuk <oleksandr.kovalchuk@cerence.com>
2020-10-18 13:51:17 +0300

    13

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-const validator = require('validator');
+const chalk = require('chalk');
 const {getNotes} = require('./note');
 
 const msg = getNotes();
-console.log('msg:', msg)
+console.log(chalk.green.inverse.bold(msg))
 
-console.log(validator.isEmail('alex@gmail.com'))
-console.log(validator.isURL('http://mtl-blade16-10.cerence.net:28661/dialog/#/dialogs/16420/develop/477397605/dtable?clientData=ALL_DATA'))
