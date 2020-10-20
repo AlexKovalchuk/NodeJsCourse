5aa0be965eb978b39709a16d07afa4da7b8373fa
OleksandrKovalchuk <oleksandr.kovalchuk@cerence.com>
2020-10-20 20:05:41 +0300

    16

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const chalk = require('chalk');
 const {getNotes} = require('./note');
 
-const msg = getNotes();
-console.log(chalk.blue.bold(msg))
+const command = process.argv[2];
+console.log(process.argv)
 
+if (command === 'add') {
+    console.log('Adding note.')
+} else if(command === 'remove') {
+    console.log('Removing note')
+}
