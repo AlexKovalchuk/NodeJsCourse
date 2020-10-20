18dbff1f270c46883b95508d359fcfa5600add5e
OleksandrKovalchuk <oleksandr.kovalchuk@cerence.com>
2020-10-20 20:33:59 +0300

    17

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const {getNotes} = require('./note');
 
-const command = process.argv[2];
-console.log(process.argv)
+yargs.version('1.1.0')
 
-if (command === 'add') {
-    console.log('Adding note.')
-} else if(command === 'remove') {
-    console.log('Removing note')
-}
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('Adding a new note');
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing a new note');
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List notes',
+    handler: function() {
+        console.log('Listing notes');
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read notes',
+    handler: function() {
+        console.log('Reading notes');
+    }
+})
+
+console.log(yargs.argv)
