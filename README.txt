7ca7b690c2ccf77492e374bfc2657ddbe121d322
AlexanderKovalchuk <oleksandr.kovalchuk@onseo.biz>
2020-10-29 21:13:28 +0200

    35

--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
 https://coursehunter.net/course/nodejs-v2-udemy
-Lesson: 31
+Lesson: 35
 
 darkssky api = 1e0017af96e5f2851dc76d38dbccd8f3
--- weather-app/app.js
+++ weather-app/app.js
@@ -4,20 +4,24 @@ const apiKey = '1e0017af96e5f2851dc76d38dbccd8f3';
 const weatherUrl = 'http://api.weatherstack.com/current'
 const access_key = 'access_key=1e0017af96e5f2851dc76d38dbccd8f3';
 const query = 'query=49.23278,28.48097';
+const qr = 'query=';
 const units = 'units=m';
 const lng = 'en';
 const url = `${weatherUrl}?${access_key}&${query}&${units}`;
 
 request({url: url, json: true}, (error, response) => {
   if(error) {
-    console.log('Error')
-    return null;
+    console.log('Error', error)
+  } else if(response.body.error) {
+    console.log('Unable to find location');
+  } else {
+    const {body} = response;
+    const {current} = body
+    const {feelslike, temperature} = current
+    console.log(`It is currently ${temperature} degress out. It feels like ${feelslike} degress out.`)
   }
 
-  const {body} = response;
-  const {current} = body
-  const {feelslike, temperature} = current
-  console.log(`It is currently ${temperature} degress out. It feels like ${feelslike} degress out.`)
+
 });
 
 // Geocoding
@@ -28,13 +32,16 @@ const limit = 'limit=1';
 const mapbox = `${mapbox_url}${mapbox_token}&${limit}`
 request({url: mapbox, json: true}, (error, response) => {
   if(error) {
-    console.log('Error')
+    console.log('Error: unable to connect to server...', error)
     return null;
+  } else if(!response.body.features.length) {
+    console.log('Unable to find location');
+  } else {
+    const {features} = response.body;
+    const {center} = features[0];
+    const latitude = center[1]
+    const longtitude = center[0]
+    console.log(`Latitude: ${latitude} and longtitude: ${longtitude}`)
   }
 
-  const {features} = response.body;
-  const {center, geometry} = features[0];
-  const latitude = center[1]
-  const longtitude = center[0]
-  console.log(`Latitude: ${latitude} and longtitude: ${longtitude}`)
 });
