bd0e439ec9b134ca02206bc0e873962255315a56
AlexanderKovalchuk <oleksandr.kovalchuk@onseo.biz>
2020-11-03 18:24:33 +0200

    39

--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
 https://coursehunter.net/course/nodejs-v2-udemy
-Lesson: 38
+Lesson: 39
 
 darkssky api = 1e0017af96e5f2851dc76d38dbccd8f3
--- weather-app/app.js
+++ weather-app/app.js
@@ -1,13 +1,23 @@
 const {geocode} = require('./utils/geocode');
 const {forecast} = require('./utils/forecast');
 
-geocode('Kyiv', (error, data) => {
-  console.log('Error:', error);
-  console.log('Data:', data);
+const locationInput = process.argv[2];
+if(!locationInput) return console.log('Please provide address.')
+
+geocode(locationInput, (error, data) => {
+  if(error) {
+    return console.log('Error:', error);
+  }
+
+    console.log('Location:', data.location);
+    const {latitude, longtitude} = data
+    forecast(latitude, longtitude, (error, forecastData) => {
+      if (error) return console.log('Error:', error);
+      
+      console.log(forecastData);
+    })
+
 });
 
-forecast(50.25, 30.5, (error, data) => {
-  console.log('Error:', error);
-  console.log('Data:', data);
-})
+
 
