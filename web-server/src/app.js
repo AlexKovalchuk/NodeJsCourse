ce2cd16012d9c4e83f122098402cb8f71c5d4cfb
OleksandrKovalchuk <oleksandr.kovalchuk@cerence.com>
2020-11-09 18:43:26 +0200

    47 in progress

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,23 @@ const express = require('express');
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 const app = express()
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req,res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Oleks Kovalchuk'
+  })
+});
+
+app.get('/about', (req,res) => {
+  res.render('about', {
+    title: 'About Page',
+    name: 'Oleks Kovalchuk'
+  })
+});
+
 
 app.get('/weather', (req, res) => {
   res.send({
