43ccf8adaffc7480cee5583dc645a070010f153e
OleksandrKovalchuk <oleksandr.kovalchuk@cerence.com>
2020-11-11 17:11:01 +0200

    8

--- express.js
+++ express.js
@@ -7,12 +7,15 @@ const todos = [
 ];
 
 const server = http.createServer((req,res) => {
-    res.setHeader('Content-Type', 'applicaton/json')
-    res.setHeader('X-Powered-By', 'Node.js')
+    res.writeHead(404, {
+        'Content-Type': 'applicaton/json',
+        'X-Powered-By': 'Node.js'
+    })
     
     res.end(JSON.stringify({
-        success: true,
-        data: todos,
+        success: false,
+        error: 'Not Found',
+        data: null,
     }));
 });
 
