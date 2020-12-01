d6776da4973786179c86b22ae25e68ce47fbe3e8
AlexanderKovalchuk <oleksandr.kovalchuk@onseo.biz>
2020-12-01 21:03:21 +0200

    first model

--- README.txt
+++ README.txt
@@ -3,5 +3,5 @@ https://coursehunter.net/course/node-js-api-master-klass-s-express-i-mongodb
 https://github.com/bradtraversy/devcamper-api
 
 
-Lesson: 16 - install mongodb in Mac
 Lesson 17 - install mongoose in PC
+Lessonn 21 - MacOs
--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 const {logger} = require('./middleware/logger');
-// Routes files
-
-const bootcamps = require('./routes/bootcamps');
+const {connectDB} = require('./config/db');
 
 // Load env vars
 dotenv.config({path: './config/config.env'});
+
+// Connect to DB
+connectDB();
+
+// Routes files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -23,4 +28,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+  PORT, 
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+);
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Unhandled Rejection ${err.message}`.red);
+
+  server.close(() => process.exit(1));
+})
